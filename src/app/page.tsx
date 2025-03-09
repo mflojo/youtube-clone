4eac6d9c246a5e885287d53070c1e08f0bbc47d2
mflojo <m.eric.flojo@gmail.com>
2025-03-09 13:18:13 -0700

    Import shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <div>
-      <p className="font-bold text-rose-500">
-        Hello!
-      </p>
+      <Button>
+        Click me
+      </Button>
     </div>
   );
 }
