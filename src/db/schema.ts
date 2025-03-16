1aec84e6268753c7c21df94330e3f4e0d2659f0f
mflojo <m.eric.flojo@gmail.com>
2025-03-16 11:31:01 -0700

    Add video edit form

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,6 @@
 import { relations } from "drizzle-orm";
-import { integer, pgEnum, pgTable, text, timestamp, uniqueIndex, uuid } from "drizzle-orm/pg-core";
+import { integer, pgEnum, pgTable, text, timestamp, uniqueIndex, uuid, } from "drizzle-orm/pg-core";
+import { createInsertSchema, createSelectSchema, createUpdateSchema, } from "drizzle-zod"
 
 export const users = pgTable("users", {
    id: uuid("id").primaryKey().defaultRandom(), 
@@ -55,6 +56,10 @@ export const videos = pgTable("videos", {
    createdAt: timestamp("created_at").defaultNow().notNull(),
    updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
+
+export const videoInsertSchema = createInsertSchema(videos);
+export const videoUpdateSchema = createUpdateSchema(videos);
+export const videoSelectSchema = createSelectSchema(videos);
 
 export const videoRelations = relations(videos, ({ one }) => ({
    user: one(users, {
