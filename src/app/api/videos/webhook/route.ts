44cdee8f721f243e7fae8b72e22278c6b4d48166
mflojo <m.eric.flojo@gmail.com>
2025-03-15 22:00:43 -0700

    Add video delete/subtitle webhook

--- src/app/api/videos/webhook/route.ts
+++ src/app/api/videos/webhook/route.ts
@@ -5,6 +5,7 @@ import {
     VideoAssetErroredWebhookEvent,
     VideoAssetReadyWebhookEvent,
     VideoAssetTrackReadyWebhookEvent,
+    VideoAssetDeletedWebhookEvent,
 } from "@mux/mux-node/resources/webhooks";
 
 import { mux } from "@/lib/mux";
@@ -17,7 +18,8 @@ type WebhookEvent =
     | VideoAssetCreatedWebhookEvent
     | VideoAssetErroredWebhookEvent
     | VideoAssetReadyWebhookEvent
-    | VideoAssetTrackReadyWebhookEvent;
+    | VideoAssetTrackReadyWebhookEvent
+    | VideoAssetDeletedWebhookEvent;
 
 export const POST = async (request: Request) => {
     if (!SIGNING_SECRET) {
@@ -50,6 +52,8 @@ export const POST = async (request: Request) => {
                 return new Response("No upload ID found", { status: 400 });
             }
 
+            console.log("Creating video: ", { uploadId: data.upload_id });
+
             await db
                 .update(videos)
                 .set({
@@ -73,6 +77,7 @@ export const POST = async (request: Request) => {
 
             const thumbnailUrl = `https://image.mux.com/${playbackId}/thumbnail.jpg`;
             const previewUrl = `https://image.mux.com/${playbackId}/animated.gif`;
+
             const duration = data.duration ? Math.round(data.duration * 1000) : 0;
 
             await db
@@ -86,6 +91,60 @@ export const POST = async (request: Request) => {
                     duration,
                 })
                 .where(eq(videos.muxUploadId, data.upload_id));
+            break;
+        }
+        case "video.asset.errored": {
+            const data = payload.data as VideoAssetErroredWebhookEvent["data"];
+
+            if (!data.upload_id) {
+                return new Response("Missing upload ID", { status: 400 });
+            }
+
+            await db
+                .update(videos)
+                .set({
+                    muxStatus: data.status,
+                })
+                .where(eq(videos.muxUploadId, data.upload_id));
+            break;
+        }
+        case "video.asset.deleted": {
+            const data = payload.data as VideoAssetDeletedWebhookEvent["data"];
+
+            if (!data.upload_id) {
+                return new Response("Missing upload ID", { status: 400 });
+            }
+
+            console.log("Deleting video: ", { uploadId: data.upload_id });
+
+            await db
+                .delete(videos)
+                .where(eq(videos.muxUploadId, data.upload_id));
+            break;
+        }
+        case "video.asset.track.ready": {
+            const data = payload.data as VideoAssetTrackReadyWebhookEvent["data"] & {
+                asset_id: string;
+            }
+
+            console.log("Track ready");
+
+            // Typescript incorrectly says asset_id does not exist
+            const assetId = data.asset_id;
+            const trackId = data.id;
+            const status = data.status;
+
+            if (!assetId) {
+                return new Response("Missing asset ID", { status: 400 });
+            }
+
+            await db
+                .update(videos)
+                .set({
+                    muxTrackId: trackId,
+                    muxTrackStatus: status,
+                })
+                .where(eq(videos.muxAssetId, assetId))
             break;
         }
     }
--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -4,6 +4,7 @@ import { InfiniteScroll } from "@/components/infinite-scroll";
 import { DEFAULT_LIMIT } from "@/constants";
 import { trpc } from "@/trpc/client";
 import { Suspense } from "react";
+import { format } from "date-fns";
 import { ErrorBoundary } from "react-error-boundary";
 import {
     Table,
@@ -15,6 +16,8 @@ import {
 } from "@/components/ui/table";
 import Link from "next/link";
 import { VideoThumbnail } from "@/modules/videos/ui/components/video-thumbnail";
+import { snakeCaseToTitle } from "@/lib/utils";
+import { Globe2Icon, LockIcon } from "lucide-react";
 
 export const VideosSection = () => {
     return (
@@ -62,16 +65,31 @@ const VideoSectionSuspense = () => {
                                                     duration={video.duration || 0}
                                                 />
                                             </div>
+                                            <div className="flex flex-col overflow-hidden gap-y-1">
+                                                <span className="text-sm line-clamp-1">{video.title}</span>
+                                                <span className="text-xs text-muted-foreground line-clamp-1">
+                                                    {video.description || "No description"}
+                                                </span>
+                                            </div>
                                         </div>
                                     </TableCell>
                                     <TableCell>
-                                        Visibility
+                                        <div className="flex items-center">
+                                            {video.visibility === "private" ? (
+                                                <LockIcon className="size-4 mr-2" />
+                                            ): (
+                                                <Globe2Icon className="size-4 mr-2" />
+                                            )}
+                                            {snakeCaseToTitle(video.visibility)}
+                                        </div>
                                     </TableCell>
                                     <TableCell>
-                                        Status
+                                        <div className="flex items-center">
+                                            {snakeCaseToTitle(video.muxStatus || "error")}
+                                        </div>
                                     </TableCell>
-                                    <TableCell>
-                                        Date
+                                    <TableCell className="text-sm truncate">
+                                        {format(new Date(video.createdAt), "MMM dd yyyy")}
                                     </TableCell>
                                     <TableCell>
                                         Views
