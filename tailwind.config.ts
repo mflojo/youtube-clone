d4fb2c43f4f2152c0aa04e779581fc1c46e3c326
mflojo <m.eric.flojo@gmail.com>
2025-03-16 18:54:05 -0700

    Add thumbnail functionality via UploadThing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from "tailwindcss";
+import { withUt } from "uploadthing/tw";
 
-export default {
+
+
+export default withUt({
     darkMode: ["class"],
     content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -92,4 +95,4 @@ export default {
   	}
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+}) satisfies Config;
