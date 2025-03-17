d03295ad6d070828d58682fcc18a91461bff6941
mflojo <m.eric.flojo@gmail.com>
2025-03-16 19:58:35 -0700

    Add thumbnail cleanup

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -3,7 +3,7 @@ import { users, videos } from "@/db/schema";
 import { auth } from "@clerk/nextjs/server";
 import { eq, and } from "drizzle-orm";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
-import { UploadThingError } from "uploadthing/server";
+import { UploadThingError, UTApi } from "uploadthing/server";
 import { z } from "zod";
 
 const f = createUploadthing();
@@ -30,6 +30,31 @@ export const ourFileRouter = {
 
         if (!user) throw new UploadThingError("Unauthorized");
 
+        const [existingVideo] = await db
+            .select({
+              thumbnailKey: videos.thumbnailKey,
+          })
+            .from(videos)
+            .where(and(
+                eq(videos.id, input.videoId),
+                eq(videos.userId, user.id),
+            ));
+
+        if (!existingVideo) throw new UploadThingError("Not found");
+        
+        if (existingVideo.thumbnailKey) {
+            const utapi = new UTApi();
+
+            await utapi.deleteFiles(existingVideo.thumbnailKey);
+            await db
+                .update(videos)
+                .set({ thumbnailKey: null, thumbnailUrl: null })
+                .where(and(
+                    eq(videos.id, input.videoId),
+                    eq(videos.userId, user.id),
+                ));
+        };
+
       return { user, ...input };
     })
     .onUploadComplete(async ({ metadata, file }) => {
@@ -37,6 +62,7 @@ export const ourFileRouter = {
             .update(videos)
             .set({
                 thumbnailUrl: file.url,
+                thumbnailKey: file.key,
             })
             .where(and(
                 eq(videos.id, metadata.videoId),
